d7112ece739fe4447254575a88bdd3bbc2a9ec91
wonu606 <wonu606@gmail.com>
2023-08-11 15:06:48 +0900

    fix: lombok 의존성 제거

--- design-patterns/code/src/main/java/com/wonu606/decoratorpattern/persistencemanager/User.java
+++ design-patterns/code/src/main/java/com/wonu606/decoratorpattern/persistencemanager/User.java
@@ -1,15 +1,33 @@
 package com.wonu606.decoratorpattern.persistencemanager;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-@AllArgsConstructor
-@Data
 public class User {
 
-    private String name;
-    private String phoneNumber;
-    private String address;
+    private final String name;
+    private final String phoneNumber;
+    private final String address;
+
+    public User(String name, String phoneNumber, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name + "\n"
+                + "phoneNumber: " + phoneNumber + "\n"
+                + "address: " + address + "\n";
+    }
 }
